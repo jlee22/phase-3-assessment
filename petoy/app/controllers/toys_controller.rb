15cce94e30bd94216d663e74b063faf49e1b4e4e
Jeffrey Lee <email": null>
2016-11-02 10:46:26 -0700

    implement create toy feature under pet index woo

--- petoy/app/controllers/toys_controller.rb
+++ petoy/app/controllers/toys_controller.rb
@@ -10,4 +10,28 @@ class ToysController < ApplicationController
     @toy = Toy.find(params[:id])
   end
 
+  def new
+    @pet = Pet.find(params[:pet_id])
+    @toy = Toy.new
+  end
+
+  def create
+    @toy = Toy.new(toy_params)
+    @pet = Pet.find(params[:pet_id])
+    @toy.pet = @pet
+    if @toy.save
+      redirect_to pet_toy_path(@pet, @toy)
+    else
+      @errors = @toy.errors.full_messages
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def toy_params
+    params.require(:toy).permit(:description)
+  end
+
 end
