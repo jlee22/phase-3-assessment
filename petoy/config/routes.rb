057e27873a137de52092ff2872d36363286aa322
Jeffrey Lee <email": null>
2016-11-02 09:26:38 -0700

    add routes, nest toy under pets and g controllers for the two models

--- petoy/config/routes.rb
+++ petoy/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pet#index'
+
+  resources :pet, only: :index do
+    resources :toys
+  end
+
 end
