bb82410db398bbad119e729b1cc35f167f7d6644
Jeffrey Lee <email": null>
2016-11-02 09:50:13 -0700

    add margin 20px to left of body, add toy show and toy index, modify controller to hit those views

--- petoy/app/controllers/toys_controller.rb
+++ petoy/app/controllers/toys_controller.rb
@@ -1,11 +1,12 @@
 class ToysController < ApplicationController
 
   def index
-
+    @pet = Pet.find(params[:pet_id])
+    @toys = @pet.toys
   end
 
   def show
-
+    @toy = Toy.find(params[:id])
   end
 
 end
